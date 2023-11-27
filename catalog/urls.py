bce4f79712898560da19257b52c43e0354a20a64
munquzbek <munchik31@gmail.com>
2023-11-27 00:20:01 -0500

    Hw 3

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from catalog.views import home, contact
+from catalog.apps import CatalogConfig
+from catalog.views import home, contact, products
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home),
-    path('contact', contact),
+    path('', home, name='home'),
+    path('contact/', contact, name='contact'),
+    path('products/', products, name='products'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    context = {
+        'title': 'Home page'
+    }
+    return render(request, 'catalog/home.html', context)
 
 
 def contact(request):
-    return render(request, 'catalog/contact.html')
+    context = {
+        'title': 'Contact page'
+    }
+    return render(request, 'catalog/contact.html', context)
+
+
+def products(request):
+    product_list = Product.objects.all()
+    context = {
+        'product_list': product_list,
+        'title': 'Products page'
+    }
+    return render(request, 'catalog/products.html', context)
