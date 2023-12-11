ed3ba5e47685fbed2d8d25be0949778315e99af9
munquzbek <munchik31@gmail.com>
2023-12-10 22:54:29 -0500

    FROM FBV to CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import home, contact, products, product
+from catalog.views import home, contact, ProductListView, ProductDetailView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
     path('contact/', contact, name='contact'),
-    path('products/', products, name='products'),
-    path('product/<int:pk>', product, name='product'),
+    path('products/', ProductListView.as_view(), name='products'),
+    path('product/<int:pk>', ProductDetailView.as_view(), name='product'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from catalog.models import Product
 
@@ -16,20 +17,31 @@ def contact(request):
     }
     return render(request, 'catalog/contact.html', context)
 
+# Function Based View
+# def products(request):
+#     product_list = Product.objects.all()
+#     context = {
+#         'product_list': product_list,
+#         'title': 'Products page'
+#     }
+#     return render(request, 'catalog/product_list.html', context)
 
-def products(request):
-    product_list = Product.objects.all()
-    context = {
-        'product_list': product_list,
-        'title': 'Products page'
-    }
-    return render(request, 'catalog/products.html', context)
+
+# Class Based View the same as 'def products(request):' (previous function)
+class ProductListView(ListView):
+    model = Product
+
+# FBV
+# def product(request, pk):
+# GET request
+#     product = Product.objects.get(pk=pk)
+#     context = {
+#         'product': product
+#     }
+#     return render(request, 'catalog/product_detail.html', context)
 
 
-def product(request, pk):
-    product = Product.objects.get(pk=pk)
-    context = {
-        'product': product
-    }
-    return render(request, 'catalog/product.html', context)
+# CBV
+class ProductDetailView(DetailView):
+    model = Product
 
